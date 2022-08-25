b792967375c3b059a437106a7dfa227d3963e09a
amalkv123 <amalkvarghese2019@gmail.com>
2022-08-25 16:42:13 +0530

    third commit

--- app/urls.py
+++ app/urls.py
@@ -16,6 +16,16 @@ urlpatterns = [
     path('profitgroup',views.profitgroup,name='profitgroup'),
     path('expence',views.expence,name='expence'),
     path('expensemonth/<int:pk>',views.expensemonth,name='expensemonth'),
+    path('purchase',views.purchase,name='purchase'),
+    path('purchasemonth/<int:pk>',views.purchasemonth,name='purchasemonth'),
+    path('indirect',views.indirect,name='indirect'),
+    path('indirectmonth/<int:pk>',views.indirectmonth,name='indirectmonth'),
+    path('stockgroup',views.stockgroup,name='stockgroup'),
+    path('stock_groups',views.stock_groups,name='stock_groups'),
+    path('stockitem',views.stockitem,name='stockitem'),
+    path('stock_items',views.stock_items,name='stock_items'),
+    path('ledger',views.ledger,name='ledger'),
+    path('save_ledger',views.save_ledger,name='save_ledger'),
    
 
 
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 import os
-from app.models import crtcompony,create_payhead,compute_information,Rounding,gratuity
+from app.models import crtcompony,create_payhead,compute_information,Rounding,gratuity,CreateStockGrp,stock_item_crt,Ledger,Ledger_Banking_Details,Ledger_Mailing_Address,Ledger_Tax_Register,Ledger_Satutory,Ledger_Rounding,ledger_tax,Ledger_sundry
 from django.contrib.auth.models import auth,User
 from django.contrib import messages
 
@@ -64,7 +64,6 @@ def selectcompony(request):
     return render(request,'selectcompony.html',{'data':data})
 
 
-#payroll
 
 
 
@@ -83,11 +82,229 @@ def expensemonth(request,pk):
     data=create_payhead.objects.get(id=pk)
     return render(request,'expensemonth.html',{'p':data})
 
+def purchase(request):
+    return render(request, 'purchase.html')
+
+def purchasemonth(request,pk):
+    return render(request, 'purchasemonth.html')
+
+def indirect(request):
+    return render(request, 'indirect.html')
+
+def indirectmonth(request,pk):
+    return render(request, 'indirectmonth.html')
+
 def stockgroup(request):
     return render(request, 'stockgroup.html')
 
+
+
+
+    #ledger
+
+
+
 def ledger(request):
     return render(request,'ledger.html')
+
+def save_ledger(request):
+    if request.method == 'POST':
+        # Ledger Basic
+        Lname = request.POST.get('ledger_name', False)
+        Lalias = request.POST.get('ledger_alias', False)
+        Lunder = request.POST.get('group_under', False)
+        Lopening_bal = request.POST.get('ledger_opening_bal', False)
+        typ_of_ledg = request.POST.get('ledger_type', False)
+        provide_banking = request.POST.get('provide_banking_details', False)
+
+        # Banking_details
+        B_od_limit = request.POST.get('od_limit', False)
+        B_ac_holder_name =request.POST.get('holder_name', False)
+        B_ac_no = request.POST.get('ac_number', False)
+        B_ifsc = request.POST.get('ifsc', False)
+        B_swift_code =request.POST.get('swift_code', False)
+        B_name = request.POST.get('bank_name', False)
+        B_branch = request.POST.get('branch_name', False)
+        B_alter_chq_bks =request.POST.get('alter_chk_bks', False)
+        B_name_enbl_chq_prtg = request.POST.get('enbl_chk_printing', False) 
+        B_chqconfg= request.POST.get('chqconfg', False) 
+        # Mailing_details
+        Mname = request.POST.get('name', False)
+        Maddress = request.POST.get('address', False)
+        Mstate =request.POST.get('state', False)
+        Mcountry = request.POST.get('country', False)
+        Mpincode = request.POST.get('pincode', False)
+
+        # Tax_Registration_Details
+        Tgst_uin = request.POST.get('gst_uin', False)
+        Treg_typ = request.POST.get('register_type', False)
+        Tpan_no = request.POST.get('pan_no', False)
+        T_alter_gst =request.POST.get('alter_gst_details', False)
+
+        # Satutory Details
+        assessable_calculationn = request.POST.get('assessable_calculation', False)
+        Appropriate_too =request.POST.get('Appropriate_to', False)
+        gst_applicablee = request.POST.get('is_gst_applicable',False)
+        Set_alter_GSTT=request.POST.get('Set_alter_GST', False)
+        type_of_supplyy = request.POST.get('type_of_supply',False)
+        Method_of_calcc=request.POST.get('Method_of_calc', False)
+
+        #leadger Rounding
+        ledger_idd=request.POST.get('useadvc', False)
+        Rounding_Methodd=request.POST.get('Rounding_Method', False)
+        Round_limitt =request.POST.get('Round_limit', False)
+
+        #ledger_tax 
+        type_of_duty_or_taxx=request.POST.get('type_of_duty_or_tax', False)
+        type_of_taxx=request.POST.get('type_of_tax', False)
+        valuation_typee=request.POST.get('valuation_type', False)
+        rate_per_unitt=request.POST.get('rate_per_unit', False)
+        Persentage_of_calculationn=request.POST.get('Persentage_of_calculation', False)
+
+        #sundry
+        maintain_balance_bill_by_billl=request.POST.get('maintain_balance_bill_by_bill', False)
+        Default_credit_periodd=request.POST.get('Default_credit_period', False)
+        Check_for_credit_dayss=request.POST.get('Check_for_credit_days', False)
+
+        if Ledger.objects.filter(ledger_name = Lname ).exists():
+                messages.info(request,'This Name is already taken...!')
+                return redirect('load_create_ledger.html')
+
+        Lmdl = Ledger(
+            ledger_name=Lname,
+            ledger_alias=Lalias,
+            group_under=Lunder,
+            ledger_opening_bal=Lopening_bal,
+            ledger_type=typ_of_ledg,
+            provide_banking_details=provide_banking,
+        )
+        Lmdl.save()
+        idd = Lmdl
+        Bmdl = Ledger_Banking_Details(
+        
+            ledger_id=idd,
+            od_limit=B_od_limit,
+            holder_name=B_ac_holder_name,
+            ac_number=B_ac_no,
+            ifsc=B_ifsc,
+            swift_code=B_swift_code,
+            bank_name=B_name,
+            branch_name=B_branch,
+            alter_chk_bks=B_alter_chq_bks,
+            enbl_chk_printing=B_name_enbl_chq_prtg,
+
+        )
+        Bmdl.save()
+        M_mdl = Ledger_Mailing_Address(
+
+            name=Mname,
+            address=Maddress,
+            state=Mstate,
+            country=Mcountry,
+            pincode=Mpincode,
+        )
+        M_mdl.save()
+        T_mdl = Ledger_Tax_Register(
+           
+          
+            gst_uin=Tgst_uin,
+            register_type=Treg_typ,
+            pan_no=Tpan_no,
+            alter_gst_details=T_alter_gst,
+
+        )
+        T_mdl.save()
+        LS_mdl = Ledger_Satutory(
+
+            ledger_id=idd,
+            assessable_calculation=assessable_calculationn,
+            Appropriate_to =Appropriate_too ,
+            gst_applicable=gst_applicablee,
+            Set_alter_GST = Set_alter_GSTT,
+            type_of_supply=type_of_supplyy,
+            Method_of_calc = Method_of_calcc,
+
+
+        )
+        LS_mdl.save()
+
+        rnd_mdl = Ledger_Rounding(
+            ledger_id=idd,
+            Rounding_Method=Rounding_Methodd,
+            Round_limit =Round_limitt,
+
+        )
+        rnd_mdl.save()
+
+        tax_mdl = ledger_tax(
+            ledger_id=idd,
+            type_of_duty_or_tax=type_of_duty_or_taxx,
+            type_of_tax =type_of_taxx,
+            valuation_type=valuation_typee,
+            rate_per_unit=rate_per_unitt,
+            Persentage_of_calculation=Persentage_of_calculationn,
+        )
+        tax_mdl.save()
+
+        sndry_mdl = Ledger_sundry(
+            ledger_id=idd,
+            maintain_balance_bill_by_bill=maintain_balance_bill_by_billl,
+            Default_credit_period=Default_credit_periodd,
+            Check_for_credit_days =Check_for_credit_dayss,
+        )
+        sndry_mdl.save()
+        messages.info(request,'LEDGER CREATED SUCCESSFULLY')
+        return redirect('ledger.html')
+
+
+
+#stock
+
+def stockgroup(request):
+    std=CreateStockGrp.objects.all()
+    return render(request,'stockgroup.html',{'std':std})
+
+
+def stock_groups(request):
+    und=CreateStockGrp.objects.all()
+    if request.method=='POST':
+        name=request.POST['name']
+        alias=request.POST['alias']
+        under_name=request.POST['under_name']
+        quantities=request.POST['quantities']
+        stockgrp=CreateStockGrp(name=name,alias=alias,under_name=under_name,quantities=quantities)
+        stockgrp.save()
+        return redirect('stock_items')
+    return render(request,'group_stock.html',{'und':und})    
+
+def stockitem(request):
+    std=CreateStockGrp.objects.all()
+    return render(request,'stockitem.html',{'std':std})
+
+def stock_items(request):
+    grp=CreateStockGrp.objects.all()
+    if request.method=='POST':
+        name=request.POST['name']
+        alias=request.POST['alias']
+        under=request.POST['under']
+        # category=request.POST['category',FALSE]
+        units=request.POST['units']
+        batches=request.POST['batches']
+        manufacturing_date=request.POST['manufacturing_date']
+        expiry_dates=request.POST['expiry_dates']
+        rate_of_duty=request.POST['rate_of_duty']
+        quantity=request.POST['quantity']
+        rate=request.POST['rate']
+        per=request.POST['per']
+        value=request.POST['value']
+        additional=request.POST['additional']
+        crt=stock_item_crt(name=name,alias=alias,under=under,units=units,batches=batches,
+                           manufacturing_date=manufacturing_date,expiry_dates=expiry_dates,
+                           rate_of_duty=rate_of_duty,quantity=quantity,rate=rate,per=per,value=value,additional=additional)
+        crt.save()
+        return redirect('stock_item')
+    return render(request,'stockitem.html',{'grp':grp})
+
 
 
 
